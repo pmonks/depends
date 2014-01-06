7955cb600a71190fc2994c247a25b71b56679d32
Peter Monks <pmonks@gmail.com>
2014-01-06 11:47:19 -0800

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -17,10 +17,10 @@
   :dependencies [
                   [org.clojure/clojure                  "1.5.1"]
                   [org.clojure/data.json                "0.2.3"]
-                  [org.clojure/tools.cli                "0.2.4"]
+                  [org.clojure/tools.cli                "0.3.1"]
                   [org.clojure/tools.trace              "0.7.6"]
                   [org.clojure/tools.logging            "0.2.6"]
-                  [clojurewerkz/neocons                 "2.0.0"]
+                  [clojurewerkz/neocons                 "2.0.1"]
                   [ch.qos.logback/logback-classic       "1.0.13"]
                   [org.ow2.asm/asm                      "4.2"]
                   [net.java.truevfs/truevfs-kernel-impl "0.10.5"]  ; Ugh TrueVFS' dependencies are crap
@@ -28,10 +28,10 @@
                   [net.java.truevfs/truevfs-driver-file "0.10.5"]
                   [net.java.truevfs/truevfs-driver-zip  "0.10.5"]
                   [net.java.truevfs/truevfs-driver-jar  "0.10.5"]
-                  [lacij                                "0.8.2" :exclusions [org.clojure/clojure]]
+                  [lacij                                "0.9.0" :exclusions [org.clojure/clojure]]
                 ]
   :profiles {:dev {:dependencies [
-                                   [midje "1.5.1"]
+                                   [midje "1.6.0"]
                                    [clj-ns-browser "1.3.1"]
                                  ]}}
   :jvm-opts ^:replace []  ; Stop Leiningen from turning off JVM optimisations - makes it slower to start but ensures code runs as fast as possible
