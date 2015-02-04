cbc31f50d0f39a538130010da6126d928f9b3dd1
Peter Monks <pmonks@gmail.com>
2015-02-04 11:07:49 -0800

    Updated dependencies

--- project.clj
+++ project.clj
@@ -23,7 +23,7 @@
                   [org.clojure/tools.logging                    "0.3.1"]
                   [clojurewerkz/neocons                         "3.0.0"]
                   [ch.qos.logback/logback-classic               "1.1.2"]
-                  [io.aviso/pretty                              "0.1.14"]
+                  [io.aviso/pretty                              "0.1.16"]
                   [org.ow2.asm/asm                              "5.0.3"]
                   [net.java.truevfs/truevfs-kernel-impl         "0.10.8"]  ; Ugh TrueVFS' dependencies are crap
                   [net.java.truevfs/truevfs-access              "0.10.8"]
