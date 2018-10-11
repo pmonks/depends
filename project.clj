51bb0f4a4180565519d56c2abaf5bb67bd8e270e
Peter Monks <pmonks@gmail.com>
2018-10-11 11:21:46 -0700

    :arrow_up: Upgrade dependencies

--- project.clj
+++ project.clj
@@ -14,17 +14,17 @@
   :url              "https://github.com/pmonks/depends"
   :license          {:name "Eclipse Public License"
                      :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.5.0"
-  :javac-target     "1.7"
+  :min-lein-version "2.8.1"
+  :javac-target     "1.8"
   :dependencies [
-                  [org.clojure/clojure                          "1.8.0"]
+                  [org.clojure/clojure                          "1.9.0"]
                   [org.clojure/data.json                        "0.2.6"]
-                  [org.clojure/tools.cli                        "0.3.5"]
-                  [org.clojure/tools.logging                    "0.4.0"]
-                  [clojurewerkz/neocons                         "3.1.0"]
+                  [org.clojure/tools.cli                        "0.4.1"]
+                  [org.clojure/tools.logging                    "0.4.1"]
+                  [clojurewerkz/neocons                         "3.2.0"]
                   [ch.qos.logback/logback-classic               "1.2.3"]
-                  [io.aviso/pretty                              "0.1.34"]
-                  [org.ow2.asm/asm                              "6.0"]
+                  [io.aviso/pretty                              "0.1.35"]
+                  [org.ow2.asm/asm                              "6.2.1"]
                   [net.java.truevfs/truevfs-kernel-impl         "0.11.1"]  ; Ugh TrueVFS' dependencies are crap
                   [net.java.truevfs/truevfs-access              "0.12.0"]
                   [net.java.truevfs/truevfs-driver-file         "0.12.0"]
@@ -32,10 +32,7 @@
                   [net.java.truevfs/truevfs-driver-jar          "0.12.0"]
                   [net.java.truecommons/truecommons-key-disable "2.5.0"]
                 ]
-  :profiles {:dev {:dependencies [[midje      "1.8.3"]]
-                   :plugins      [[lein-midje "3.2.1"]]}   ; Don't remove this or travis-ci will assplode!
+  :profiles {:dev {:dependencies [[midje      "1.9.3"]]
+                   :plugins      [[lein-midje "3.2.1"]]}
               :uberjar {:aot :all}}
-  :uberjar-exclusions [#".*\.disabled"]
-  :jar-exclusions     [#".*\.disabled"]
-  :jvm-opts           ["-Xmx4g" "-server"]
   :main depends.main)
